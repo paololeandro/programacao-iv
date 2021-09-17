5fbe297ef14d4f7193149b43e3b67741ae6b80ac
paololeandro <dipaolosantos@gmail.com>
2021-09-17 12:15:58 -0300

    fix: edit pom.xml

---
+++ src/test/java/com/programacao-iv/programacao-ivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacao-iv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class programacao-ivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
